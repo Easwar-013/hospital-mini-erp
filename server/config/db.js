eec1a400134589102bb27fff04ec57cb1b8f043a
Easwar-013 <pondyeashwar@gmai.com>
2026-07-01 21:10:44 +0530

    Fix MongoDB environment variable

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed");
-    console.error(error.message);
+    console.log("❌ MongoDB Connection Failed");
+    console.log(error.message);
     process.exit(1);
   }
 };
